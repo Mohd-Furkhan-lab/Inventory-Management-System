23e666f18ee6d34e051993582566f86e58564688
Mohd-Furkhan-lab <mohdfurkhan2255@gmail.com>
2026-04-04 21:16:46 +0530

    Change database paths to use /tmp directory

--- db/database.py
+++ db/database.py
@@ -2,7 +2,7 @@ import sqlite3
 
 
 def get_product_db():
-    return sqlite3.connect("Products.db")
+    return sqlite3.connect("/tmp/Products.db")
 
 def get_admin_db():
-    return sqlite3.connect("Admin.db")
+    return sqlite3.connect("/tmp/Admin.db")
