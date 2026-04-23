ab15d6336b7b4412e0e7426743f691757f06d36b
Mohd Furkhan <mohdfurkhan7755@gmial.com>
2026-04-23 21:10:17 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from app.main import get_app
-
+import os
 app = get_app()
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
